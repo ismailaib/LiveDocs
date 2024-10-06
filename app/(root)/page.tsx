f2dce0c8811249758d9515dc8a91d91f243032f6
ismailaib <ismailaitbouhmad@gmail.com>
2024-10-06 15:09:50 +0100

    Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,44 @@
-import { Button } from '@/components/ui/button'
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/dist/server/api-utils';
+import Image from 'next/image'
+const home = async () => {
+  const clerkUser = await currentUser()
 
-const home = () => {
+  if (!clerkUser) redirect('/sign-in');
+
+  const documents = [];
   return (
-    <div>
-      <Button>Click Here</Button>
-    </div>
+    <main className='home-container'>
+      <Header className="sticky left-0 top-0">
+        <div className='flex items-center gap-2 lg:gap-4'>
+          Notification 
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div>
+            
+        </div>
+      ) : 
+      <div className='document-list-empty'>
+        <Image 
+          src="/assets/icons/doc.svg"
+          alt='Doc'
+          width={40}
+          height={40}
+          className='mx-auto'
+        />
+
+        <AddDocumentBtn userId={clerkUser.id} email={clerkUser?.emailAddresses[0].emailAddress} />
+      </div>
+      }
+    </main>
   )
 }
 
